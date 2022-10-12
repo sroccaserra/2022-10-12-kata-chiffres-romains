35fd4aabe51bc9a6de8058f8a7a1f45889ed4089
sroccaserra <sroccaserra@yahoo.com>
2022-10-12 16:25:52 +0200

    Ajoute la doc de ce qu'on a compris

--- src/ConvertisseurDeChiffresRomains.java
+++ src/ConvertisseurDeChiffresRomains.java
@@ -12,6 +12,16 @@ public class ConvertisseurDeChiffresRomains {
 
         //////////////////
 
+        /**
+         * cette partie là ajoute le bon nombre de I
+         * si i = 1, le bon nombre de bâtons c'est 1
+         *        2                                2
+         *        3                                3
+         *        4                                x
+         *        5                                0 = 5 - 5
+         *        6                                1 = 6 - 5
+         *        7                                2 = 7 - 5
+         */
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < nombre; ++i) {
             result.append("I");
